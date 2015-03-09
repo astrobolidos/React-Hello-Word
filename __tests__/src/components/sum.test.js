78e63892849c9914cbf76176b965c12fc43da3dc
astrobolidos <lisandroalavez@gmail.com>
2015-03-09 13:54:45 +0000

    reordered __tests__directory

--- __tests__/src/components/sum.test.js
+++ __tests__/src/components/sum.test.js
@@ -3,8 +3,7 @@ var path = '../../../js/components/sum.js'
 jest.dontMock(path);
 
 describe('sum', function() {
-	it('adds 1 + 2 to equal 3', function() {#
-
+	it('adds 1 + 2 to equal 3', function() {
 
 		var sum = require(path);
 		expect(sum(1, 2)).toBe(3);
